2e823c35394934b5a5576da8e026e11273588877
Michal Kupczak <kurczak12333@gmail.com>
2022-11-27 19:25:20 +0100

    Added map and locations on a map

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { useState } from "react";
 
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
+
+  const handleCoordinates = async () => {
+    const response = await fetch(
+      "https://portal-api-prod-etflayreta-ew.a.run.app/MobileApi"
+    );
+    fetch("https://portal-api-prod-etflayreta-ew.a.run.app/MobileApi")
+      .then((response) => response.json())
+      .then((data) => console.log(`data`, data));
+
+    const data = await response.json();
+    setCoordinates(data);
+  };
+
+  useEffect(() => {
+    handleCoordinates();
+  }, []);
+
+  if (!coordinates) {
+    return <></>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 49.783572,
+          longitude: 19.05895,
+          latitudeDelta: 0.0005,
+          longitudeDelta: 0.0005,
+        }}
+      >
+        {coordinates.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: marker.geolocation.latitude,
+              longitude: marker.geolocation.longitude,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01,
+            }}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
     </View>
   );
 }
@@ -13,8 +58,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
