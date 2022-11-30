cefd1927ba647ed6089d56c7fccac8cda55eae02
Michal Kupczak <kurczak12333@gmail.com>
2022-11-30 16:59:42 +0100

    Fixed app

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import { useEffect } from "react";
-import { StyleSheet, SafeAreaView, Text } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+} from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { useState, useCallback } from "react";
 import * as Location from "expo-location";
 
 export default function App() {
   const [coordinates, setCoordinates] = useState();
-  const [nearestPointText, setNearestPointText] = useState("");
+  const [nearestPoint, setNearestPoint] = useState("");
 
   const handleCoordinates = useCallback(async () => {
     const response = await fetch(
       "https://portal-api-prod-etflayreta-ew.a.run.app/MobileApi"
     );
     const data = await response.json();
-    setCoordinates(data);
+    const mappedDate = data.map((item) => {
+      return {
+        ...item,
+        shouldNotTrack: false,
+      };
+    });
+    setCoordinates(mappedDate);
     const { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== "granted") {
       return;
@@ -42,25 +55,36 @@ export default function App() {
       {
         // Tracking options
         accuracy: Location.Accuracy.High,
-        distanceInterval: 10,
+        distanceInterval: 100,
       },
       (location) => {
         if (coordinates) {
-          coordinates.forEach((item) => {
-            const distance = calcCrow(
-              item.geolocation.latitude,
-              item.geolocation.longitude,
-              location.coords.latitude,
-              location.coords.longitude
-            );
-            if (distance < 30 && item.shouldNotTrack === undefined) {
-              return setNearestPointText(item.locatlizationName);
-            }
-          });
+          setNearestPointMethod(location);
         }
       }
     );
   }, [coordinates]);
+
+  const setNearestPointMethod = (location) => {
+    let nearestPoint = [];
+    coordinates.forEach((item) => {
+      const distance = calcCrow(
+        item.geolocation.latitude,
+        item.geolocation.longitude,
+        location.coords.latitude,
+        location.coords.longitude
+      );
+      if (distance < 30 && item.shouldNotTrack === false) {
+        nearestPoint = [...nearestPoint, { distance: distance, item: item }];
+      }
+    });
+    if (nearestPoint.length > 0) {
+      const lowestDistance = nearestPoint.reduce((prev, current) =>
+        prev.distance < current.distance ? prev : current
+      );
+      return setNearestPoint(lowestDistance);
+    }
+  };
 
   // Converts numeric degrees to radians
   const toRad = (Value) => {
@@ -83,6 +107,13 @@ export default function App() {
     return d;
   };
 
+  const removeCoordinate = () => {
+    setCoordinates((prev) => {
+      return prev.filter((item) => item !== nearestPoint.item);
+    });
+    setNearestPoint("");
+  };
+
   if (!coordinates) {
     return <></>;
   }
@@ -99,20 +130,55 @@ export default function App() {
         }}
       >
         {coordinates.map((marker, index) => (
-          <Marker
-            key={index}
-            coordinate={{
-              latitude: marker.geolocation.latitude,
-              longitude: marker.geolocation.longitude,
-              latitudeDelta: 0.002,
-              longitudeDelta: 0.002,
-            }}
-            title={marker.title}
-            description={marker.description}
-          />
+          <>
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: marker.geolocation.latitude,
+                longitude: marker.geolocation.longitude,
+                latitudeDelta: 0.002,
+                longitudeDelta: 0.002,
+              }}
+              title={marker.title}
+              description={marker.description}
+            />
+          </>
         ))}
       </MapView>
-      <Text>{nearestPointText}</Text>
+      {nearestPoint && nearestPoint.item.shouldNotTrack === false && (
+        <View style={styles.text}>
+          <Text>{nearestPoint && nearestPoint.item.question}</Text>
+          <View style={styles.button}>
+            <Button
+              onPress={removeCoordinate}
+              title={nearestPoint && nearestPoint.item.answer}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              style={styles.button}
+              onPress={removeCoordinate}
+              title={nearestPoint && nearestPoint.item.answer}
+            />
+          </View>
+
+          <View style={styles.button}>
+            <Button
+              style={styles.button}
+              onPress={removeCoordinate}
+              title={nearestPoint && nearestPoint.item.answer}
+            />
+          </View>
+
+          <View style={styles.button}>
+            <Button
+              style={styles.button}
+              onPress={removeCoordinate}
+              title={nearestPoint && nearestPoint.item.answer}
+            />
+          </View>
+        </View>
+      )}
     </SafeAreaView>
   );
 }
@@ -123,5 +189,20 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
+  },
+  text: {
+    textAlign: "center",
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  layout: {
+    width: "100%",
+    height: "100%",
+  },
+  button: {
+    margin: 10,
   },
 });
